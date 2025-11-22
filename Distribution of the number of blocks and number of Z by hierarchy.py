f4a821a4b31c5eaad0c549f7d3f18ed8f4b410c1
FIDY HERITIANA ANDRIANARIVONY <heritianafidy@gmail.com>
2025-11-22 22:55:43 +0300

    Add files via upload

---
+++ Distribution of the number of blocks and number of Z by hierarchy.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "56b1ba6d-0b84-4ab9-bfbc-e3c7859537c9",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ Random generation of an oriented hierarchy structure.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b5ae664b-2c1d-4b0f-a5fa-5fef72f0cdb7",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
